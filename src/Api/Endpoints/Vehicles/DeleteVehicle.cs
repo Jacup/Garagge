82c2af61f825962971381430618baa2a4d77857a
Jakub Gramburg <jacubgr@gmail.com>
2025-07-31 20:48:16 +0200

    Add DeleteVehicle

--- src/Api/Endpoints/Vehicles/DeleteVehicle.cs
+++ src/Api/Endpoints/Vehicles/DeleteVehicle.cs
@@ -1,6 +1,25 @@
+using Api.Endpoints.Users;
+using Api.Extensions;
+using Api.Infrastructure;
+using Application.Core;
+using Application.Vehicles.DeleteMyVehicleById;
+using MediatR;
+
 namespace Api.Endpoints.Vehicles;
 
-public class DeleteVehicle
+public class DeleteVehicle : IEndpoint
 {
-    
+    public void MapEndpoint(IEndpointRouteBuilder app)
+    {
+        app.MapPost("/vehicles/my/{id:guid}", async (Guid id, ISender sender, CancellationToken cancellationToken) =>
+            {
+                var command = new DeleteMyVehicleByIdCommand(id);
+                
+                Result<bool> result = await sender.Send(command, cancellationToken);
+
+                return result.Match(Results.NoContent, CustomResults.Problem);
+            })
+            .HasPermission(Permissions.UsersAccess)
+            .WithTags(Tags.Vehicles);
+    }
 }
