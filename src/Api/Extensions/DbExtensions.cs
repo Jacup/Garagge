37f94e1d48ebbb29e7b74390fdaa1c58b673a5e8
Jakub Gramburg <jacubgr@gmail.com>
2025-08-03 14:45:21 +0200

    Add database seeding and refactor migration extensions
    Introduced DatabaseSeeder to seed initial users and vehicles in development. Replaced MigrationExtensions with DbExtensions, consolidating migration and seeding logic. Updated Program.cs to seed the database after applying migrations.

---
+++ src/Api/Extensions/DbExtensions.cs
@@ -0,0 +1,31 @@
+﻿using Infrastructure.DAL;
+using Infrastructure.Database;
+using Microsoft.EntityFrameworkCore;
+
+namespace Api.Extensions;
+
+public static class DbExtensions
+{
+    public static void ApplyMigrations(this IApplicationBuilder app)
+    {
+        using IServiceScope scope = app.ApplicationServices.CreateScope();
+
+        using ApplicationDbContext dbContext = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+
+        dbContext.Database.Migrate();
+    }
+
+    public static async Task SeedDatabaseAsync(this IApplicationBuilder app)
+    {
+        using IServiceScope scope = app.ApplicationServices.CreateScope();
+        
+        var environment = scope.ServiceProvider.GetRequiredService<IWebHostEnvironment>();
+        
+        if (!environment.IsDevelopment())
+            return;
+
+        await using ApplicationDbContext dbContext = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+        
+        await DatabaseSeeder.SeedAsync(dbContext, scope.ServiceProvider);
+    }
+}
