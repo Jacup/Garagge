b2f7566a35a3f3f520d484626c1bc7efb5c1eff0
Jakub Gramburg <jacubgr@gmail.com>
2025-08-17 23:03:34 +0200

    Refactor layout to use Vuetify navigation drawers

--- garagge-web/src/App.vue
+++ garagge-web/src/App.vue
@@ -1,13 +1,32 @@
 <script lang="ts" setup>
-import MainContent from './components/layout/MainContent.vue'
-import NavBar from './components/layout/NavBar.vue'
+import { ref, computed } from 'vue'
+import { useDisplay } from 'vuetify'
+import AppNavigation from './components/layout/AppNavigation.vue'
+
+const { name } = useDisplay()
+const drawer = ref(false)
+
+const isDesktop = computed(() => ['lg', 'xl', 'xxl'].includes(name.value))
+const isTablet = computed(() => name.value === 'md')
+const isMobile = computed(() => ['sm', 'xs'].includes(name.value))
 </script>
 
 <template>
-  <div class="app-layout">
-    <NavBar />
-    <MainContent />
-  </div>
+  <v-app>
+    <v-navigation-drawer v-if="isDesktop" permanent :width="240"> </v-navigation-drawer>
+
+    <v-navigation-drawer v-else-if="isTablet" rail permanent :width="72"> </v-navigation-drawer>
+
+    <v-navigation-drawer v-else v-model="drawer" temporary> </v-navigation-drawer>
+
+    <v-app-bar title="Application bar"></v-app-bar>
+
+    <v-main class="d-flex align-center justify-center" height="300">
+      <v-container>
+        <v-sheet color="surface-light" height="200" rounded="lg" width="100%"></v-sheet>
+      </v-container>
+    </v-main>
+  </v-app>
 </template>
 
 <style scoped>
