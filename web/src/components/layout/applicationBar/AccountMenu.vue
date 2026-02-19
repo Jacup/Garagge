f2e298d13f161b5b33cb57dec0cf5758a1213258
Jakub Gramburg <jacubgr@gmail.com>
2026-02-19 09:41:46 +0100

    Update AccountMenu.vue

--- web/src/components/layout/applicationBar/AccountMenu.vue
+++ web/src/components/layout/applicationBar/AccountMenu.vue
@@ -31,10 +31,10 @@ const handleLogout = async () => {
       <template v-slot:title> {{ userStore.fullName }} </template>
       <template v-slot:subtitle>{{ userStore.profile?.email }}</template>
 
-      <v-list class="md3-list">
-        <v-list-item class="md3-list-item" disabled prepend-icon="mdi-account" to="/profile" title="Profile" />
-        <v-list-item class="md3-list-item" prepend-icon="mdi-cog" to="/settings" title="Settings" />
-        <v-list-item class="md3-list-item" prepend-icon="mdi-wrench" to="/administration-settings" title="Administration" />
+      <v-list rounded class="material-list">
+        <v-list-item :active="false" disabled prepend-icon="mdi-account" to="/profile" title="Profile" />
+        <v-list-item :active="false" prepend-icon="mdi-cog" to="/settings" title="Settings" />
+        <v-list-item :active="false" prepend-icon="mdi-wrench" to="/administration-settings" title="Administration" />
       </v-list>
       <v-card-actions class="pt-4 pb-0 px-0">
         <v-btn variant="tonal" prepend-icon="mdi-logout" block @click="handleLogout"> Logout </v-btn>
